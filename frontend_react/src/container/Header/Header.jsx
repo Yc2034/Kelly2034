d17ba9d48a58a76e3c1f6f6018cc7e98547dddad
MollyMiao <peiyaoshi0321@gmail.com>
2022-09-22 15:33:08 -0400

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -28,13 +28,13 @@ const Header = () => (
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Peiyao</h1>
           </div>
         </div>
 
         <div className="tag-cmp app__flex">
-          <p className="p-text">Web Developer</p>
-          <p className="p-text">Freelancer</p>
+          <p className="p-text">Software Engineer</p>
+          <p className="p-text">Data Lover</p>
         </div>
       </div>
     </motion.div>
